bfa81c3770925b7a26b309de0cc562ab570c8aa5
worhol <trashuries@gmail.com>
2021-02-15 11:12:35 -0600

    Added method to read bodies from a file

--- skeleton-sp19/proj0/NBody.java
+++ skeleton-sp19/proj0/NBody.java
@@ -6,4 +6,19 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    public static Body[] readBodies(String fileName){
+            In in = new In(fileName);
+           int first = in.readInt();
+           double second = in.readDouble();
+           Body[] bodies = new Body[5];
+           for(int i=0;i<bodies.length;i++){
+               bodies[i]=new Body(in.readDouble(),in.readDouble(),
+                   in.readDouble(),in.readDouble(),in.readDouble(),in.readString());
+           }
+
+
+            return bodies;
+
+    }
 }
